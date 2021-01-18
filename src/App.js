0faf31477b8b7e9a83527226072541a53247478d
Wali ullah <wali190939@gmail.com>
2021-01-19 04:38:56 +0600

    Slider created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import 'antd/dist/antd.css';
+
 import './App.css';
+
 import Slider from './components/Slider.jsx';
 
 
@@ -7,6 +9,7 @@ function App() {
   return (
   
      <Slider/>
+
  
  
   );

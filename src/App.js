f1b9918b1a4f2e09bde080a8e4ce3dadbd7e741e
Wali ullah <wali190939@gmail.com>
2021-01-19 23:46:55 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import 'antd/dist/antd.css';
-import './App.css';
-import Slider from './components/Slider.jsx';
-
+import "antd/dist/antd.css";
+import "./App.css";
+import Slider from "./components/Slider.jsx";
 
 function App() {
-  return (
-  
-   <Slider/>
- 
-  );
+  return <Slider />;
 }
 
 export default App;

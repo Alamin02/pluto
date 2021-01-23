4c37a8f48130b8f05a65f1de16965f38a0527a31
Md. Al Amin <alamin.ece.ku@gmail.com>
2021-01-23 20:51:09 +0600

    Change order

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
-import React from 'react';
-import 'antd/dist/antd.css';
-import Features from './components/popular-products/Features'
+import React from "react";
+import "antd/dist/antd.css";
+import Features from "./components/popular-products/Features";
 
 import "antd/dist/antd.css";
 import "./App.css";
@@ -9,16 +8,12 @@ import "./App.css";
 import Slider from "./components/Slider.js";
 import Navbar from "./components/Navbar";
 
-
 function App() {
   return (
     <div>
-
-      <Features />
-
       <Navbar />
-      <Slider />;
-
+      <Slider />
+      <Features />
     </div>
   );
 }

1024112e77d416a4064478e3e5e9ed1463e327d3
raihankabir36850 <67608675+raihankabir36850@users.noreply.github.com>
2021-01-26 13:14:23 +0600

    update product list

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -12,7 +12,7 @@ import image7 from "../../assets/images/denim-tshirt.jpg";
 import image8 from "../../assets/images/full-black.jpg";
 
 import CardItem from "./ProductCard";
-import TitleHolder from './TitleHolder';
+import Header from './Header';
 import ViewButton from './Button'
 
 const itemList = [
@@ -69,7 +69,7 @@ const itemList = [
 function ProductList() {
   return (
     <div className={styles.containerFluid}>
-      <TitleHolder title='popular products' />
+      <Header title='popular products' />
       <Row gutter={[16, 16]}>
         {itemList.map((item) => (
           <Col span={6} key={item.id}>

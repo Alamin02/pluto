611f13134b555df2d2eee663662d6c1864e463ec
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-01-28 01:04:07 +0600

    added error 404 page & routing

---
+++ src/components/error-404/Error404.js
@@ -0,0 +1,30 @@
+import { Button } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+
+import styles from "./Error404.module.css";
+import ShockedMan from "./404.gif";
+
+function Error404() {
+  return (
+    <div className={styles.container}>
+      <img src={ShockedMan} alt="ShockedMan" className={styles.gifStyle} />
+      <p className={styles.fourOFourStyle}>Error 404!</p>
+      <p>
+        <div className={styles.firstParagraphStyle}>
+          Looks like you are lost.
+        </div>
+        <br />
+        The page you are looking for is not available!
+      </p>
+      <Button
+        type="primary"
+        className={styles.buttonStyle}
+        icon={<HomeOutlined />}
+      >
+        Go back to home page
+      </Button>
+    </div>
+  );
+}
+
+export default Error404;

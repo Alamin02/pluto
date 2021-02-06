573337f090107b64a88e3fb7e01f1acb011ddf6b
Wali ullah <wali190939@gmail.com>
2021-02-06 18:19:07 +0600

    refractor: components name updated props used

---
+++ src/components/product-view/PriceAndSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+import styles from "./ProductView.module.css";
+
+export default function PriceAndSummary(props) {
+  const { summaryText, productName, ProductPrice } = props;
+  return (
+    <div>
+      <div className={styles.marginBottom}>
+        <h1>{productName}</h1>
+        <h2>
+          <b>BDT&nbsp;{ProductPrice}</b>
+        </h2>
+        <p>{summaryText}</p>
+      </div>
+      <AddToCart />
+    </div>
+  );
+}

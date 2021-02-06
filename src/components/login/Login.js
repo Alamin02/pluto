29d36cc37f2e329158bf742d3d395fd2d8beb039
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-06 12:48:35 +0600

    Login component done

---
+++ src/components/login/Login.js
@@ -0,0 +1,91 @@
+import { Form, Input, Button, Checkbox } from "antd";
+
+import styles from "./Login.module.css";
+import Heading from "../heading/Heading";
+
+const layout = {
+  labelCol: {
+    span: 5,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 5,
+    span: 16,
+  },
+};
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <div className={`${styles.bgColor} ${styles.block}`}>
+        <div className={styles.containerFluid}>
+          <Heading
+            headingStyle={styles.titleHolder}
+            headingTitle="Log Into Your Account"
+          />
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            style={{
+              padding: "20px",
+            }}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,15 @@ import React from "react";
 import Slider from "../components/slider/Slider";
 import FeaturedProducts from "../components/featured-products/FeaturedProducts";
 import ProductList from "../components/product/ProductList";
+import Login from "../components/login/Login";
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProducts />
-      <ProductList />
+      <Login />
+      <ProductList />      
     </div>
   );
 };

17caf99397548e3d74ad98816810cee742537a50
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-06 12:49:11 +0600

    Login component done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,12 @@ import React from "react";
 import Slider from "../components/slider/Slider";
 import FeaturedProducts from "../components/featured-products/FeaturedProducts";
 import ProductList from "../components/product/ProductList";
-import Login from "../components/login/Login";
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProducts />
-      <Login />
       <ProductList />      
     </div>
   );

acf5b0a5c24970de021b996603dd417959202477
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-07 18:43:47 +0600

    new: registration from is added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 import Slider from "../components/slider/Slider";
 import FeaturedProducts from "../components/featured-products/FeaturedProducts";
 import ProductList from "../components/product/ProductList";
+import Registration from "../components/registration/Registration"
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProducts />
+      <Registration />
       <ProductList />      
     </div>
   );

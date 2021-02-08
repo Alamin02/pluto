d133b3b5caee4b354297789541c55aa16ce213e0
Md. Al Amin <alamin.ece.ku@gmail.com>
2021-02-08 23:55:03 +0600

    fix: grid for product list

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col } from "antd";
+
 import styles from "./ProductList.module.css";
 import sampleProductData from "../../assets/data/sampleProductData";
 
@@ -14,7 +15,7 @@ function ProductList() {
       <Header title="popular products" />
       <Row gutter={[16, 16]}>
         {sampleProductData.map((item) => (
-          <Col span={6} key={item.id}>
+          <Col xxl={6} xl={8} md={12} sm={12} xs={24} key={item.id}>
             <Link to={`/products/${item.id}`}>
               <CardItem
                 title={item.productName}
@@ -27,7 +28,10 @@ function ProductList() {
       </Row>
       <div className={styles.buttonStyle}>
         <Link to="/products/list">
-          <ButtonBlack buttonText="View all products" className={styles.buttonStyle} />
+          <ButtonBlack
+            buttonText="View all products"
+            className={styles.buttonStyle}
+          />
         </Link>
       </div>
     </div>

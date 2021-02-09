97b677fd95417927c735037816f881fec8988df5
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-09 21:28:14 +0600

    new: HeaderSection moved to styled comp

---
+++ src/components/styled-components/HeaderSection.js
@@ -0,0 +1,23 @@
+import { Grid } from "antd";
+import classNames from "classnames";
+import styles from "./HeaderSection.module.css";
+
+const { useBreakpoint } = Grid;
+
+export default function HeaderSection({ headerText }) {
+  const screens = useBreakpoint();
+
+  return (
+    <div>
+      <h2
+        className={classNames(
+          { [styles.header]: screens },
+          { [styles.headerSm]: screens.xs }
+        )}
+      >
+        {headerText}
+      </h2>
+      <hr className={styles.hrStyle} />
+    </div>
+  );
+}

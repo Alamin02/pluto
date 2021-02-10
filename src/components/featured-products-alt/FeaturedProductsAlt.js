e166f38567deea8bab8e644bc745be73ec880843
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-11 00:22:30 +0600

    fix: FeaturedProductsAlt infos are now separated

--- src/components/featured-products-alt/FeaturedProductsAlt.js
+++ src/components/featured-products-alt/FeaturedProductsAlt.js
@@ -1,20 +1,10 @@
+import { Link } from "react-router-dom";
 import { Grid } from "antd";
 import classNames from "classnames";
 import styles from "./FeaturedProductsAlt.module.css";
 
 import HeaderSection from "../styled-components/HeaderSection";
-
-import image1 from "./images/image1.jpg";
-import image2 from "./images/image2.jpg";
-import image3horizontal from "./images/image3h.jpg";
-import image3vertical from "./images/image3v.jpg";
-
-const imageTitles = {
-  id: 1,
-  image1Title: "Vintage watch",
-  image2Title: "Nike shoe",
-  image3Title: "Lamp",
-};
+import featuredProductsAltInfo from "./featuredProductsAltInfo";
 
 const { useBreakpoint } = Grid;
 
@@ -41,45 +31,50 @@ function FeaturedProductsAlt() {
         >
           {/* top image of 1st flexbox container */}
           <div className={styles.firstFlexBoxTopImageContainer}>
-            <img
-              className={classNames(
-                { [styles.firstFlexBoxImage]: screens },
-                { [styles.firstFlexBoxImageXs]: screens.xs }
-              )}
-              src={image1}
-              alt=""
-            />
+            <Link to={featuredProductsAltInfo.image1Url}>
+              <img
+                className={classNames(
+                  { [styles.firstFlexBoxImage]: screens },
+                  { [styles.firstFlexBoxImageXs]: screens.xs }
+                )}
+                src={featuredProductsAltInfo.image1Src}
+                alt={featuredProductsAltInfo.image1Title}
+              />
+            </Link>
             <div
               className={classNames(
                 { [styles.imageTitle]: screens },
                 { [styles.imageTitleXs]: screens.xs }
               )}
             >
-              {imageTitles.image1Title}
+              {featuredProductsAltInfo.image1Title}
             </div>
           </div>
 
+          {/* bottom image of 1st flexbox container */}
           <div
             className={classNames(
               { [styles.firstFlexBoxBottomImageContainer]: screens },
               { [styles.firstFlexBoxBottomImageContainerXs]: screens.xs }
             )}
           >
-            <img
-              className={classNames(
-                { [styles.firstFlexBoxImage]: screens },
-                { [styles.firstFlexBoxImageXs]: screens.xs }
-              )}
-              src={image2}
-              alt=""
-            />
+            <Link to={featuredProductsAltInfo.image2Url}>
+              <img
+                className={classNames(
+                  { [styles.firstFlexBoxImage]: screens },
+                  { [styles.firstFlexBoxImageXs]: screens.xs }
+                )}
+                src={featuredProductsAltInfo.image2Src}
+                alt={featuredProductsAltInfo.image2Title}
+              />
+            </Link>
             <div
               className={classNames(
                 { [styles.imageTitle]: screens },
                 { [styles.imageTitleXs]: screens.xs }
               )}
             >
-              {imageTitles.image2Title}
+              {featuredProductsAltInfo.image2Title}
             </div>
           </div>
         </section>
@@ -92,30 +87,32 @@ function FeaturedProductsAlt() {
           )}
         >
           {/* vertical 3rd image */}
-          <img
-            className={classNames(
-              { [styles.secondFlexBoxImageH]: screens },
-              { [styles.secondFlexBoxImageHXs]: screens.xs }
-            )}
-            src={image3vertical}
-            alt=""
-          />
-          {/* horizontal 3rd image */}
-          <img
-            className={classNames(
-              { [styles.secondFlexBoxImageV]: screens },
-              { [styles.secondFlexBoxImageVXs]: screens.xs }
-            )}
-            src={image3horizontal}
-            alt=""
-          />
+          <Link to={featuredProductsAltInfo.image3Url}>
+            <img
+              className={classNames(
+                { [styles.secondFlexBoxImageH]: screens },
+                { [styles.secondFlexBoxImageHXs]: screens.xs }
+              )}
+              src={featuredProductsAltInfo.image3VSrc}
+              alt={featuredProductsAltInfo.image3Title}
+            />
+            {/* horizontal 3rd image */}
+            <img
+              className={classNames(
+                { [styles.secondFlexBoxImageV]: screens },
+                { [styles.secondFlexBoxImageVXs]: screens.xs }
+              )}
+              src={featuredProductsAltInfo.image3HSrc}
+              alt={featuredProductsAltInfo.image3Title}
+            />
+          </Link>
           <div
             className={classNames(
               { [styles.imageTitle]: screens },
               { [styles.imageTitleXs]: screens.xs }
             )}
           >
-            {imageTitles.image3Title}
+            {featuredProductsAltInfo.image3Title}
           </div>
         </section>
       </section>

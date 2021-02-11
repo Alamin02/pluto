ad53b100aa7251b9ab92e550e13f04636a47e925
Mehedi Hasan <el06@mehedi-hasan.lan>
2021-02-11 21:01:04 +0600

    Add: Login page is added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import BlogPage from "./pages/BlogPage";
 import Footer from "./components/footer/Footer";
 import Error404 from "./components/error-404/Error404";
 import RegistrationPage from "./pages/Registration";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/checkOut" component={CheckOut} exact />
         <Route path="/blogs" component={BlogPage} exact />
         <Route path="/registration" component={RegistrationPage} exact />
+        <Route path="/login" component={LoginPage}/>
         <Route component={Error404} />
       </Switch>
       <Footer />

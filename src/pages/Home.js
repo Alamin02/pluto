026545760b031f309868886827dee878049baca1
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-12 23:01:23 +0600

    fix: FeaturedProductsALt is now shown on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Slider from "../components/slider/Slider";
-import FeaturedProducts from "../components/featured-products/FeaturedProducts";
 import ProductList from "../components/product/ProductList";
 import FeaturedProductsAlt from "../components/featured-products-alt/FeaturedProductsAlt";
 
@@ -9,8 +8,7 @@ const Home = () => {
   return (
     <div>
       <Slider />
-      <FeaturedProducts />
-      {/* <FeaturedProductsAlt /> */}
+      <FeaturedProductsAlt />
       <ProductList />
     </div>
   );

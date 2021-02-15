54bd360ea5c29efb0f40aa3d266505a1253052ee
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-15 17:57:37 +0600

    fix: fixed ContactUs page style and related comp styles

--- src/components/contact-us/MessageForm.js
+++ src/components/contact-us/MessageForm.js
@@ -1,86 +1,95 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import { Typography } from 'antd';
-import { Row, Col, Grid } from 'antd';
-import { Input } from 'antd';
-import styles from './MessageForm.module.css'
-import ButtonBlack from '../styled-components/ButtonBlack'
+import { Typography } from "antd";
+import { Row, Col, Grid } from "antd";
+import { Input } from "antd";
+import styles from "./MessageForm.module.css";
+import ButtonBlack from "../styled-components/ButtonBlack";
 import classNames from "classnames";
 
 const { useBreakpoint } = Grid;
 const { TextArea } = Input;
 const { Title, Text } = Typography;
 const labelStyle = {
-    marginBottom: '10px',
-    color: '#808080',
-    fontSize: '15px',
-    fontWeight: 'bold',
-}
+  marginBottom: "10px",
+  color: "#808080",
+  fontSize: "15px",
+  fontWeight: "bold",
+};
 const textAreaStyle = {
-    marginBottom: '20px'
-}
+  marginBottom: "20px",
+};
 
 function MessageForm() {
-    const screens = useBreakpoint();
-    return (
-        <div className={styles.contactUsContainer}>
-            <Row gutter={[16, 16]}>
-                <Col span={12} xs={24} sm={24} md={24} lg={12}>
-                    <Title className={classNames(
-                        { [styles.contactTitle]: screens },
-                        { [styles.contactTitleXs]: screens.xs },
-                        { [styles.contactTitleMd]: screens.sm },
-                        { [styles.contactTitleMd]: screens.md },
-                        { [styles.contactTitleLg]: screens.lg },
-                    )}>Message Us</Title>
-                    <p>If you wish to be considered for employment at Pluto, please do not send a message, here – instead, please complete Pluto's job application  and our Human Resources department will contact you after their review of your submitted information</p>
-                </Col>
-                <Col span={12} xs={24} sm={24} md={24} lg={12}>
-                    <Row gutter={[16, 16]}>
-                        <Col span={12} xs={24} sm={12}>
-                            <div style={labelStyle}>
-                                <label htmlFor='Name'  >Name</label>
-                            </div>
-                            <div>
-                                <Input size="large" />
-                            </div>
-                            <Text type="secondary">First</Text>
-                        </Col>
-                        <Col span={12} xs={24} sm={12} className={classNames(
-                            { [styles.lastLabelStyle]: screens.xs }
-                        )}>
-                            <div className={styles.lastLabel}>
-                                <Input size="large" />
-                            </div>
-                            <Text type="secondary">Last</Text>
-                        </Col>
-                        <Col span={12} xs={24} sm={24} md={12}>
-                            <div style={labelStyle}>
-                                <label htmlFor='email'>Email</label>
-                            </div>
-                            <div>
-                                <Input size="large" type="email" />
-                            </div>
-                        </Col>
-                    </Row>
-                    <div style={labelStyle} >
-                        <label htmlFor='Comments'>Comments</label>
-                    </div>
-                    <div style={textAreaStyle}>
-                        <TextArea rows={4} />
-                    </div>
-                    <div className={styles.buttonStyle}>
-                        <Link to="#">
-                            <ButtonBlack
-                                buttonText="Submit"
-                                className={styles.buttonStyle}
-                            />
-                        </Link>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-    )
+  const screens = useBreakpoint();
+  return (
+    <div className={styles.contactUsContainer}>
+      <Row gutter={[16, 16]}>
+        <Col span={12} xs={24} sm={24} md={24} lg={12}>
+          <Title
+            className={classNames(
+              { [styles.contactTitle]: screens },
+              { [styles.contactTitleXs]: screens.xs },
+              { [styles.contactTitleMd]: screens.sm },
+              { [styles.contactTitleMd]: screens.md },
+              { [styles.contactTitleLg]: screens.lg }
+            )}
+          >
+            Message Us
+          </Title>
+          <p className={styles.longMessageText}>
+            If you wish to be considered for employment at Pluto, please do not
+            send a message, here – instead, please complete Pluto's job
+            application and our Human Resources department will contact you
+            after their review of your submitted information
+          </p>
+        </Col>
+        <Col span={12} xs={24} sm={24} md={24} lg={12}>
+          <Row gutter={[16, 16]}>
+            <Col span={12} xs={24} sm={12}>
+              <div style={labelStyle}>
+                <label htmlFor="Name">Name</label>
+              </div>
+              <div>
+                <Input size="large" />
+              </div>
+              <Text type="secondary">First</Text>
+            </Col>
+            <Col
+              span={12}
+              xs={24}
+              sm={12}
+              className={classNames({ [styles.lastLabelStyle]: screens.xs })}
+            >
+              <div className={styles.lastLabel}>
+                <Input size="large" />
+              </div>
+              <Text type="secondary">Last</Text>
+            </Col>
+            <Col span={12} xs={24} sm={24} md={12}>
+              <div style={labelStyle}>
+                <label htmlFor="email">Email</label>
+              </div>
+              <div>
+                <Input size="large" type="email" />
+              </div>
+            </Col>
+          </Row>
+          <div style={labelStyle}>
+            <label htmlFor="Comments">Comments</label>
+          </div>
+          <div style={textAreaStyle}>
+            <TextArea rows={4} />
+          </div>
+          <div className={styles.buttonStyle}>
+            <Link to="#">
+              <ButtonBlack buttonText="Submit" className={styles.buttonStyle} />
+            </Link>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessageForm;
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import MessageForm from "../components/contact-us/MessageForm"
+import React from "react";
+import MessageForm from "../components/contact-us/MessageForm";
 import MainHeader from "../components/main-header/MainHeader";
-import GetInTouch from "../components/contact-us/GetInTouch"
+import GetInTouch from "../components/contact-us/GetInTouch";
+import HeaderSection from "../components/styled-components/HeaderSection";
+
+const container = {
+  maxWidth: "1200px",
+  margin: "0 auto",
+  marginTop: "1rem",
+  padding: "0 1rem",
+};
+
+const getInTouchContainer = {
+  padding: "10px 100px",
+};
+
 function ContactUs() {
-    return (
-        <div>
-            <MainHeader name="Get In Touch" sub="" />
-            <GetInTouch />
-            <MessageForm />
-        </div>
-    )
+  return (
+    <div style={container}>
+      {/* <MainHeader name="Get In Touch" sub="" /> */}
+      <HeaderSection headerText="get in touch" />
+      <div style={getInTouchContainer}>
+        <GetInTouch />
+      </div>
+      <br />
+      <MessageForm />
+    </div>
+  );
 }
 export default ContactUs;

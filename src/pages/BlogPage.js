d4bb7be99f60aa8d69b21b94008b29459da7ed0b
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-02-15 17:57:13 +0600

    fix: fixed Blog page style and related comp styles

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,13 +2,24 @@ import React from "react";
 import Blogs from "../components/blogs/Blogs";
 import BreadCrumb from "../components/blogs/BreadCrumb";
 import MainHeader from "../components/main-header/MainHeader";
+import HeaderSection from "../components/styled-components/HeaderSection";
+
+const container = {
+  maxWidth: "1200px",
+  margin: "0 auto",
+  marginTop: "1rem",
+  padding: "0 1rem",
+};
 
 export default function BlogPage() {
   return (
     <>
-      <MainHeader name="Latest News" sub="home - shop - Blogs" />
-      <Blogs />
-      <BreadCrumb />
+      <div style={container}>
+        {/* <MainHeader name="Latest News" sub="home - shop - Blogs" /> */}
+        <HeaderSection headerText="Latest news" />
+        <Blogs />
+        <BreadCrumb />
+      </div>
     </>
   );
 }

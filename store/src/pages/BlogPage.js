9551bb99ba07a0349fb1a0540841789d2da7e79f
Wali ullah <wali190939@gmail.com>
2021-02-19 16:03:00 +0600

    fix: blogs style

--- store/src/pages/BlogPage.js
+++ store/src/pages/BlogPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Blogs from "../components/blogs/Blogs";
 import BreadCrumb from "../components/blogs/BreadCrumb";
-// import MainHeader from "../components/main-header/MainHeader";
+
 import HeaderSection from "../components/styled-components/HeaderSection";
 import appStyles from "../App.module.css";
 
@@ -9,7 +9,7 @@ export default function BlogPage() {
   return (
     <>
       <div className={appStyles.containerMain}>
-        <div>
+        <div style={{ marginTop: "1rem" }}>
           <HeaderSection headerText="Latest news" />
           <Blogs />
           <BreadCrumb />

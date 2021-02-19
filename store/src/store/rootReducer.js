b5da2d2ab41b528c3e7f7f01fae36438fed1aeb8
Wali ullah <wali190939@gmail.com>
2021-02-19 11:36:06 +0600

    fix: removed unnecessary reducer

--- store/src/store/rootReducer.js
+++ store/src/store/rootReducer.js
@@ -1,9 +1,3 @@
 import { combineReducers } from "redux";
-
 import { cartReducer } from "./cartReducer";
-import incrementDecrementReducer from "./incrementDecrementReducer";
-
-export default combineReducers({
-  cart: cartReducer,
-  update: incrementDecrementReducer,
-});
+export default combineReducers({ cart: cartReducer });

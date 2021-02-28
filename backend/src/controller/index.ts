2f8447f22b5aecc5e0bebc48ff52fa1d097b3b9b
raihankabir36850 <raihanku320@gmail.com>
2021-02-28 21:54:16 +0600

    indentation problem fixed

--- backend/src/controller/index.ts
+++ backend/src/controller/index.ts
@@ -1,21 +1,21 @@
 export {
-    userLogin as userLoginController,
-    userRegistration as userRegistrationController,
-    users as usersController,
+  userLogin as userLoginController,
+  userRegistration as userRegistrationController,
+  users as usersController,
   } from "./auth";
 
 export {
-    createProduct as createProductController,
-    getAllProducts as getAllProductsController,
-    getProduct as getProductController,
-    updateProduct as updateProductController,
-    deleteProduct as deleteProductController,
+  createProduct as createProductController,
+  getAllProducts as getAllProductsController,
+  getProduct as getProductController,
+  updateProduct as updateProductController,
+  deleteProduct as deleteProductController,
   } from "./products";
 
 export{ 
-    createBlog as createBlogController,
-    getAllBlogs as getAllBlogsController,
-    getSingleBlog as getSingleBlogController,
-    updateSingleBlog as updateSingleBlogController,
-    deleteBlog as deleteBlogController
+  createBlog as createBlogController,
+  getAllBlogs as getAllBlogsController,
+  getSingleBlog as getSingleBlogController,
+  updateSingleBlog as updateSingleBlogController,
+  deleteBlog as deleteBlogController
 } from "./blog";

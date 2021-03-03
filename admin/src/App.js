7d2582767633fa3e79f1440a87d3d4860072ce65
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-04 01:11:33 +0600

    fix: fixed Navbar

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import "antd/dist/antd.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
---
+++ admin/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button } from "antd";
+
+// styling start
+const navbar = {
+  display: "flex",
+  flexDirection: "row",
+  backgroundColor: "#001529",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "1rem 1rem",
+};
+
+const siteLogo = {
+  marginRight: "2rem",
+  height: "40px",
+};
+
+const navLeft = {
+  display: "flex",
+  alignItems: "center",
+};
+
+const adminPanelText = {
+  display: "inline-block",
+  fontSize: "2rem",
+  color: "white",
+  textTransform: "capitalize",
+};
+
+const logout = {
+  backgroundColor: "#F1F5F9",
+  marginRight: "1rem",
+  textTransform: "capitalize",
+};
+// styling end
+
+export default function Navbar() {
+  return (
+    <div>
+      <div style={navbar}>
+        <div style={navLeft}>
+          <img
+            src="https://i.imgur.com/ld4xrld.png"
+            alt="logo"
+            style={siteLogo}
+          />
+          <div style={adminPanelText}>admin panel</div>
+        </div>
+        <div>
+          <Button style={logout} size="large">
+            logout
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}

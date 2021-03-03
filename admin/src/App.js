4a09f4b57392c7a28e4d8d7abf40895b0a7fce33
raihankabir36850 <raihanku320@gmail.com>
2021-03-04 02:58:20 +0600

    login page of admin panel created

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import "antd/dist/antd.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -6,22 +7,55 @@ import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const isLoggedIn = true;
-
-  if (!isLoggedIn) {
-    return (
-      <div>
-        <Login />
-      </div>
-    );
-  } else {
-    return (
-      <Router>
-        <Navbar />
-        <Route path="/" component={Dashboard} />
-      </Router>
-    );
+  // const isLoggedIn = true;
+  // if (!isLoggedIn) {
+  //   return (
+  //     <div>
+  //       <Login />
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <Router>
+  //       <Navbar />
+  //       <Route path="/" component={Dashboard} />
+  //     </Router>
+  //   );
+  // }
+  const adminUser = {
+    name: 'admin',
+    email: 'admin@gmail.com',
+    password: 'admin123'
   }
+  const [user, setUser] = useState({ name: "", email: "", password: "" })
+  const [error, setError] = useState("")
+
+  const login = (details) => {
+    console.log(details)
+    if (details.name === adminUser.name && details.email === adminUser.email && details.password === adminUser.password) {
+      setUser({
+        name: details.name,
+        email: details.email,
+        password: details.password
+      })
+    } else {
+      setError('Do not match')
+    }
+  }
+  return (
+    <div>
+      {(user.email !== "") ?
+        (<Router>
+          <Navbar />
+          <Route path="/" component={Dashboard} />
+        </Router>
+        ) :
+        (
+          <Login login={login} error={error} />
+        )}
+
+    </div>
+  )
 }
 
 export default App;

5bfff6d131b7d8a12cf7496895789fa59d08b145
raihankabir36850 <raihanku320@gmail.com>
2021-03-04 03:10:22 +0600

    refactoring

--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -10,11 +10,13 @@ import "antd/dist/antd.css"
 import styles from './Login.module.css'
 
 function Login({ login, error }) {
+
   const [details, setDetails] = useState({ name: "", email: "", password: "" })
   const onSubmitHandler = (e) => {
     e.preventDefault();
     login(details);
   }
+
   return (
     <form>
       <div className={styles.formInner}>
@@ -24,22 +26,40 @@ function Login({ login, error }) {
           <div className={styles.labelItem}>
             <label htmlFor="name">Name:</label>
           </div>
-
-          <Input size="large" type="text" id="name" className={styles.inputItem} onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} prefix={<UserOutlined />} />
+          <Input
+            size="large"
+            type="text"
+            id="name"
+            className={styles.inputItem}
+            onChange={e => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+            prefix={<UserOutlined />} />
         </div>
         <div className={styles.formGroup}>
           <div className={styles.labelItem}>
             <label htmlFor="email">Email:</label>
           </div>
-
-          <Input size="large" type="email" id="email" className={styles.inputItem} onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} prefix={<MailOutlined />} />
+          <Input
+            size="large"
+            type="email"
+            id="email"
+            className={styles.inputItem}
+            onChange={e => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+            prefix={<MailOutlined />} />
         </div>
         <div className={styles.formGroup}>
           <div className={styles.labelItem}>
             <label htmlFor="password">Password:</label>
           </div>
-
-          <Input size="large" type="password" id="password" className={styles.inputItem} onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} prefix={<LockOutlined />} />
+          <Input
+            size="large"
+            type="password"
+            id="password"
+            className={styles.inputItem}
+            onChange={e => setDetails({ ...details, password: e.target.value })}
+            value={details.password}
+            prefix={<LockOutlined />} />
         </div>
         <Button type="primary" size="large" onClick={onSubmitHandler}>Login</Button>
       </div>

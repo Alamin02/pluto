7938f03f19975e365b6d8b62b1ad149f70271aa6
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-03 17:19:57 +0600

    fix: removed unnecessary files

--- admin/src/pages/Admin.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Admin() {
-  return <div>admin start view</div>;
-}
--- admin/src/pages/Store.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Store() {
-  return <div>store</div>;
-}

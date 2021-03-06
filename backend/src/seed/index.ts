be4a85d219d33249af556daf62d55397235a74c9
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-03-06 18:45:10 +0600

    add: order seed data

--- backend/src/seed/index.ts
+++ backend/src/seed/index.ts
@@ -2,4 +2,4 @@ export { seedUsers } from "./users";
 export { seedAddresses } from "./addresses";
 export { seedBlogs } from "./blogs";
 export { seedProducts } from "./products";
-
+export { seedOrder } from "./orders";
---
+++ backend/src/seed/orders.ts
@@ -0,0 +1,22 @@
+import { getConnection } from "typeorm";
+import { Order } from "../entity";
+
+const orderList = [
+  {
+    status: "abc",
+    paymentMethod: "xyz",
+  },
+];
+
+export async function seedOrder() {
+  const orderRepository = getConnection().getRepository(Order);
+
+  for (const order of orderList) {
+    const newOrder = new Order();
+
+    newOrder.status = order.status;
+    newOrder.paymentMethod = order.paymentMethod;
+
+    await orderRepository.save(newOrder);
+  }
+}

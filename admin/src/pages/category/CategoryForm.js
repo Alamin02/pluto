62508fe085163916a6e4e223e5cff8b428b7cd1c
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-08 19:52:03 +0600

    new: category now can be added

--- admin/src/pages/category/CategoryForm.js
+++ admin/src/pages/category/CategoryForm.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { Modal, Form, Input } from "antd";
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Input, Select } from "antd";
 
 import { agent } from "../../helpers/agent";
 
+const { Option } = Select;
+
 export default function CategoryForm({ visible, onCreate, onCancel }) {
+  const [categoryOptions, setCategoryOptions] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    fetch("http://localhost:4000/api/v1/category", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setCategoryOptions(data);
+      });
+  }, []);
+
   const [form] = Form.useForm();
 
   return (
@@ -14,31 +33,46 @@ export default function CategoryForm({ visible, onCreate, onCancel }) {
         okText="Create"
         cancelText="Cancel"
         onCancel={onCancel}
-        onOk={() => {}}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              agent.createCategory(values).then((res) => res.json());
+              form.resetFields();
+              onCreate(values);
+            })
+            .catch((info) => {
+              console.log("Validate Failed:", info);
+            });
+        }}
       >
         <Form
           form={form}
           layout="vertical"
           name="form_in_modal"
-          initialValues={{
-            modifier: "public",
-          }}
+          // initialValues={{}}
         >
           <Form.Item
-            name="category"
-            label="Category&nbsp;:"
+            name="name"
+            label="Category name&nbsp;:"
             rules={[
               {
                 required: true,
-                message: "Please enter user email!",
+                message: "Please enter category/ sub category!",
               },
             ]}
           >
             <Input />
           </Form.Item>
 
-          <Form.Item name="subCategory" label="Sub Category&nbsp;:">
-            <Input />
+          <Form.Item name="parentId" label="Parent&nbsp;:">
+            <Select defaultValue="null">
+              {categoryOptions.map((category) => (
+                <Option value={category.id} id={category.id}>
+                  {category.name}
+                </Option>
+              ))}
+            </Select>
           </Form.Item>
         </Form>
       </Modal>

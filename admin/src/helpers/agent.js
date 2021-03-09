5c5e528f9c69d709fbb4d5745184410e4c49fa99
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-09 20:38:46 +0600

    new: added functions user view in admin

--- admin/src/helpers/agent.js
+++ admin/src/helpers/agent.js
@@ -1,6 +1,7 @@
 const baseUrl = "http://localhost:4000/api/v1";
 
 export const agent = {
+  // user
   createUser: (userData, token) => {
     return fetch(`${baseUrl}/users/register`, {
       method: "post",
@@ -12,6 +13,37 @@ export const agent = {
     });
   },
 
+  getUsers: () => {
+    return fetch(`${baseUrl}/users`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  },
+
+  editUser: (userId, token, userData) => {
+    return fetch(`${baseUrl}/users/${userId}`, {
+      method: "put",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+  },
+
+  deleteUser: (token, userId) => {
+    return fetch(`${baseUrl}/users/${userId}`, {
+      method: "delete",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+  },
+
+  // product
   updateProduct: (productId, productData, token) => {
     return fetch(`${baseUrl}/users/products/${productId}`, {
       method: "get",

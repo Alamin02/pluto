9344a971d9e5614b89f4f2d5497ab0da09439a74
raihankabir36850 <raihanku320@gmail.com>
2021-03-12 00:32:11 +0600

    fix: descriptio column

--- backend/src/controller/products.ts
+++ backend/src/controller/products.ts
@@ -17,7 +17,7 @@ export async function getAllProducts(
   const perPage: number = parseInt(<string>req.query.perPage) || 10;
 
   const [products, productCount] = await productRepository.findAndCount({
-    select: ["id", "name", "price", "summary"],
+    select: ["id", "name", "description", "price", "summary"],
     relations: ["category", "offer", "images"],
     take: perPage,
     skip: (page - 1) * perPage,

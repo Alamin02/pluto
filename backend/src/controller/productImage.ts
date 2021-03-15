380c33f17b4f90a5b0a0bb9b7df84acc1e0306cb
raihankabir36850 <raihanku320@gmail.com>
2021-03-15 21:51:48 +0600

    add : product image controller

---
+++ backend/src/controller/productImage.ts
@@ -0,0 +1,33 @@
+import express = require("express");
+import { getConnection } from "typeorm";
+import { validationResult } from "express-validator";
+import { ProductImage } from "../entity";
+
+// @GET /v1/api/products/images
+// all products images
+export async function getAllProductsImages(
+  req: express.Request,
+  res: express.Response
+) {
+  const productImageRepository = getConnection().getRepository(ProductImage);
+  const allProductsImages = await productImageRepository.find();
+  res.status(200).json({ data: allProductsImages });
+}
+
+// Get a particular product image
+export async function getSingleImage(
+  req: express.Request,
+  res: express.Response
+) {
+  const id = req.params.imageId;
+
+  const productImageRepository = getConnection().getRepository(ProductImage);
+
+  const findImageById = await productImageRepository.findOne({ id });
+
+  if (!findImageById) {
+    return res.status(400).json({ errors: [{ msg: "Image not found" }] });
+  }
+
+  res.json({ msg: "category found", data: findImageById });
+}

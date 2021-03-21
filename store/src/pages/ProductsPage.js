02eedf3c500be84fda04f913ce75bc0b8608a689
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-03-21 20:07:07 +0600

    new: show products in all products page + pagination added

--- store/src/pages/ProductsPage.js
+++ store/src/pages/ProductsPage.js
@@ -1,96 +1,100 @@
-import React from "react";
-import { Row, Col } from "antd";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Badge, Pagination } from "antd";
+import { agent } from "../helpers/agent";
+import appStyles from "../App.module.css";
 
-import MainHeader from "../components/main-header/MainHeader"
-import ProductMenu from "../components/product/ProductMenu"
-import ProductOption from "../components/product/option/ProductOption"
-import CardItem from "../components/product/ProductCard"
-import PageInfo from "../components/product/PageInfo"
-
-
-import image1 from "../assets/images/addidas.jpg";
-import image2 from "../assets/images/mango-navy.jpg";
-import image3 from "../assets/images/neck-tshirt.jpg";
-import image4 from "../assets/images/polo-tshirt.jpg";
-import image5 from "../assets/images/black.jpg";
-import image6 from "../assets/images/casual.jpg";
-import image7 from "../assets/images/denim-tshirt.jpg";
-import image8 from "../assets/images/full-black.jpg";
-
-
-const item = [
-    {
-        id: 1,
-        title: "Creative Adidas T-Shirts",
-        image: image1,
-        price: "00",
-    },
-    {
-        id: 2,
-        title: "Mango-Navy",
-        image: image2,
-        price: "157",
-    },
-    {
-        id: 3,
-        title: "Gravida Est Quis Euismod",
-        image: image3,
-        price: "170",
-    },
-    {
-        id: 4,
-        title: "Gravida Est Quis Euismod",
-        image: image4,
-        price: "170",
-    },
-    {
-        id: 5,
-        title: "Gravida Est Quis Euismod",
-        image: image5,
-        price: "170",
-    },
-    {
-        id: 6,
-        title: "Gravida Est Quis Euismod",
-        image: image6,
-        price: "170",
-    },
-    {
-        id: 7,
-        title: "Gravida Est Quis Euismod",
-        image: image7,
-        price: "170",
-    },
-    {
-        id: 8,
-        title: "Gravida Est Quis Euismod",
-        image: image8,
-        price: "170",
-    },
-];
-
+import MainHeader from "../components/main-header/MainHeader";
+import ProductMenu from "../components/product/ProductMenu";
+import ProductOption from "../components/product/option/ProductOption";
+import CardItem from "../components/product/ProductCard";
 
 const ProductsPage = () => {
-    return (
-        <div>
-            <MainHeader name='popular list' sub='home - shop - products' />
-            <ProductMenu />
-            <ProductOption />
-            <Row gutter={[16, 16]} style={{ maxWidth: '1200px', margin: '20px auto' }}>
-                {item.map((item) => (
-                    <Col span={6} key={item.id}>
-                        <CardItem
-                            title={item.title}
-                            src={item.image}
-                            price={item.price}
-                        />
-                    </Col>
-                ))}
-            </Row>
-            <PageInfo />
-        </div>
+  const [productsData, setProductsData] = useState([]);
+  const [totalProductsInfo, setTotalProductsInfo] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
-    )
-}
+  function fetchProducts() {
+    agent
+      .getProducts()
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setProductsData(data.products);
+        setTotalProductsInfo(data);
+        console.log(data);
+      });
+  }
+
+  function onChange(pageNumber) {
+    setCurrentPage(pageNumber);
+    console.log("Page: ", pageNumber);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <MainHeader name="popular list" sub="home - shop - products" />
+      <div className={appStyles.containerMain}>
+        <ProductMenu />
+        <ProductOption />
+        <br />
+        <Row gutter={[16, 16]}>
+          {productsData.map((product) => {
+            if (product.offer) {
+              return (
+                <Col xxl={6} xl={6} md={8} sm={12} xs={12} key={product.id}>
+                  <Link to={`/products/${product.id}`}>
+                    <Badge.Ribbon
+                      color="red"
+                      text={product.offer.discount + ` % off`}
+                    >
+                      <CardItem
+                        title={product.name}
+                        src={product.images[0].path}
+                        price={Math.floor(
+                          product.price -
+                            (product.price * product.offer.discount) / 100
+                        )}
+                        discount={product.price}
+                      />
+                    </Badge.Ribbon>
+                  </Link>
+                </Col>
+              );
+            } else {
+              return (
+                <Col xxl={6} xl={6} md={8} sm={12} xs={12} key={product.id}>
+                  <Link to={`/products/${product.id}`}>
+                    <CardItem
+                      title={product.name}
+                      src={product.images.length && product.images[0].path}
+                      price={product.price}
+                    />
+                  </Link>
+                </Col>
+              );
+            }
+          })}
+        </Row>
+
+        <Pagination
+          style={{ display: "flex", justifyContent: "center", margin: "50px" }}
+          showSizeChanger
+          showQuickJumper
+          defaultCurrent={1}
+          current={currentPage}
+          onChange={onChange}
+          defaultPageSize={totalProductsInfo.perPage}
+          total={totalProductsInfo.productCount}
+          showTotal={(total, range) =>
+            `${range[0]}-${range[1]} of ${total} Products`
+          }
+        />
+      </div>
+    </div>
+  );
+};
 export default ProductsPage;
-

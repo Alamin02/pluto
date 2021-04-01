cf76b33f1f5d3e6eafeecb4f75e06cba49044669
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-01 21:40:35 +0600

    new: added address panel in backend

--- admin/src/helpers/agent.js
+++ admin/src/helpers/agent.js
@@ -272,4 +272,14 @@ export const agent = {
       },
     });
   },
+
+  // address
+  getAddresses: () => {
+    return fetch(`${baseUrl}/addresses`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  },
 };

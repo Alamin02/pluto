62826e70ffdf5c6172243321c909db38b813c252
raihankabir36850 <raihanku320@gmail.com>
2021-04-02 17:52:19 +0600

    store updated

--- store/src/store/authReducer.js
+++ store/src/store/authReducer.js
@@ -1,14 +1,15 @@
 const initialState = {
   isAuthenticated: !!localStorage.getItem("token"),
+  tokenValue: ""
 };
 
 export function authReducer(state = initialState, action) {
   switch (action.type) {
     case "auth/login":
-      return { ...state, isAuthenticated: true };
+      return { ...state, isAuthenticated: true, tokenValue: action.payload };
 
     case "auth/logout":
-      return { ...state, isAuthenticated: false };
+      return { ...state, isAuthenticated: false, tokenValue: "" };
 
     default:
       return state;
--- store/src/store/rootReducer.js
+++ store/src/store/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { cartReducer } from "./cartReducer";
+import { authReducer } from "./authReducer"
 
 export default combineReducers({
   cart: cartReducer,
+  auth: authReducer
 });

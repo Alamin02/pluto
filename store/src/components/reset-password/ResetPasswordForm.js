2e06746ee2154f61812a78e6f933e4aad7b70acd
raihankabir36850 <raihanku320@gmail.com>
2021-04-09 16:22:41 +0600

    refactor reset password component

--- store/src/components/reset-password/ResetPasswordForm.js
+++ store/src/components/reset-password/ResetPasswordForm.js
@@ -1,91 +1,109 @@
-import classNames from "classnames";
+
 import { Form, Input, Button } from "antd";
-import { LockOutlined } from "@ant-design/icons";
 
 import styles from "./ResetPasswordForm.module.css";
-import Heading from "../heading/Heading";
 
 const ResetPasswordForm = () => {
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: {
+      span: 3,
+    },
+    wrapperCol: {
+      span: 2,
+    },
+  };
+  const tailLayout = {
+    labelCol: { span: 3 },
+    wrapperCol: {
+      span: 8,
+      offset: 1,
+    },
+  };
+
+  const onFinishForPassword = (values) => {
+    console.log("password: ", values)
+  }
+
   return (
-    <>
-      <div className={classNames(styles.bgColor, styles.block)}>
-        <div className={styles.containerFluid}>
-          <Heading
-            headingStyle={styles.titleHolder}
-            headingTitle="Reset your Password"
+
+
+    <Form
+      name="reset-password"
+      {...layout}
+      form={form}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinishForPassword}
+    >
+      <section className={styles.passwordSection}>
+        <h2 className={styles.passwordSectionTitle}>Update User Password</h2>
+        {/* OLd Password */}
+        <Form.Item
+          {...tailLayout}
+          name="password"
+          label="Old Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder="Password"
           />
-          <Form
-            name="reset-password"
-            className={classNames(styles.forgetPassForm, styles.containerFluid)}
-            initialValues={{
-              remember: true,
-            }}
-          >
-            {/* OLd Password */}
-            <Form.Item
-              name="password"
-              label="OLd Password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            {/* New Password */}
-            <Form.Item
-              name="password"
-              label="New Password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            {/* Confirm Password */}
-            <Form.Item
-              name="password"
-              label="Confirm Password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            {/* Button Block */}
-            <Form.Item className={styles.antColRtl}>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className={styles.loginFormButton}
-              >
-                Submit
+        </Form.Item>
+        {/* New Password */}
+        <Form.Item
+          {...tailLayout}
+          name="password"
+          label="New Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        {/* Confirm Password */}
+        <Form.Item
+          {...tailLayout}
+          name="password"
+          label="Confirm Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+
+      </section>
+
+      {/* set new password button */}
+      <Form.Item className={styles.buttonSection}>
+        <Button
+          type="primary"
+          htmlType="submit"
+        >
+          Set New Password
               </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </div>
-    </>
+      </Form.Item>
+    </Form>
+
   );
 };
 

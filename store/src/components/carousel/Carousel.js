2b33146f15bef0a8de4e0ae7e6ad81ee9e357fa6
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-04-22 16:42:08 +0600

    new: fetch carousel data from backend and show in frontend

--- store/src/components/carousel/Carousel.js
+++ store/src/components/carousel/Carousel.js
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination, Autoplay, A11y } from "swiper";
 import "swiper/swiper-bundle.css";
 
+import MainContainer from "../layout/MainContainer";
 import { agent } from "../../helpers/agent";
 import "./Carousel.css";
 
@@ -20,36 +21,48 @@ export default function Carousel() {
       .then(({ data }) => setCarousels(data));
   }, []);
 
-  return (
-    <React.Fragment>
-      <Swiper
-        navigation
-        loop
-        pagination={{ clickable: true, dynamicBullets: true }}
-        autoplay={{ delay: 3500, disableOnInteraction: true }}
-        spaceBetween={50}
-        slidesPerView={1}
-      >
-        {carousels.map((carousel) => (
-          <SwiperSlide key={carousel.title}>
-            <div>
-              <img
-                className="carousel-image"
-                src={carousel.image.path}
-                alt={carousel.title}
-              />
-              <div className="carousel-details-container">
-                <p className="carousel-title">{carousel.title}</p>
-                <p className="carousel-summary">{carousel.summary}</p>
-                <Button type="primary" style={{ textTransform: "capitalize" }}>
-                  <Link to={carousel.link}>view product</Link>
-                </Button>
+  if (!carousels || !carousels.length) {
+    return (
+      <MainContainer>
+        <div style={{ padding: "0.5rem" }}>
+          No carousel images available to show. Add them from admin panel.
+        </div>
+      </MainContainer>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Swiper
+          navigation
+          loop
+          pagination={{ clickable: true, dynamicBullets: true }}
+          autoplay={{ delay: 3500, disableOnInteraction: true }}
+          spaceBetween={50}
+          slidesPerView={1}
+        >
+          {carousels.map((carousel) => (
+            <SwiperSlide key={carousel.title}>
+              <div>
+                <img
+                  className="carousel-image"
+                  src={carousel.image.path}
+                  alt={carousel.title}
+                />
+                <div className="carousel-details-container">
+                  <p className="carousel-title">{carousel.title}</p>
+                  <p className="carousel-summary">{carousel.summary}</p>
+                  <Button
+                    type="primary"
+                    style={{ textTransform: "capitalize" }}
+                  >
+                    <Link to={carousel.link}>view product</Link>
+                  </Button>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-        ...
-      </Swiper>
-    </React.Fragment>
-  );
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </React.Fragment>
+    );
+  }
 }

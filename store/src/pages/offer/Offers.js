4d5ff8b8b16e7c9f310e004d83afd54c708b720b
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-01 11:51:09 +0600

    new: updated offers page

--- store/src/pages/offer/Offers.js
+++ store/src/pages/offer/Offers.js
@@ -1,45 +1,63 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Typography, Card } from "antd";
+import { Row, Col, Typography } from "antd";
 
 import ProductCard from "../../components/product/ProductCard";
 import MainContainer from "../../components/layout/MainContainer";
+import HeaderSection from "../../components/styled-components/HeaderSection";
 import { agent } from "../../helpers/agent";
+import styles from "./Offers.module.css";
 
 const { Title, Text } = Typography;
 
 function Offers() {
   const [offersData, setOffersData] = useState([]);
+  console.log(offersData);
 
   useEffect(() => {
     agent
       .getOffers()
       .then((res) => res.json())
       .then(({ data }) => {
-        console.log(data.offers)
-        setOffersData(data.offers)
-      })
+        console.log(data.offers);
+        setOffersData(data.offers);
+      });
   }, []);
 
   return (
     <MainContainer>
-      <div style={{ marginTop: "1rem" }}>
-        <Row gutter={[16, 16]}>
-          {offersData.map((offer) => (
-            <Col xxl={6} xl={6} md={8} sm={12} xs={12} key={offer.id}>
-              {/* <Link to={`/products/${product.id}`}> */}
-              <ProductCard
-                title={offer.name}
-                src={offer.offerImage[0].path}
-                // price={product.price}
-                discount={offer.discount}
-              />
-              {/* </Link> */}
-            </Col>
-          ))}
-        </Row>
-      </div >
-    </MainContainer >
+      <HeaderSection headerText="All offers" />
+      {offersData.length &&
+        offersData.map((offer) => (
+          <div key={offer.id} className={styles.indivudialOfferSpace}>
+            <img
+              className={styles.offerImage}
+              src={offer.offerImage[0].path}
+              alt={offer.name}
+            />
+            <div className={styles.offerInfo}>
+              <Title level={3}>{offer.name}</Title>
+              <Text>{offer.description}</Text>
+            </div>
+            <Row gutter={[16, 16]}>
+              {offer.products.map((product) => {
+                return (
+                  <Col xxl={6} xl={6} md={8} sm={12} xs={12} key={product.id}>
+                    <Link to={`/products/${product.id}`}>
+                      <ProductCard
+                        title={product.name}
+                        src={product.images[0].path}
+                        price={product.price}
+                        discount={offer.discount}
+                      />
+                    </Link>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
+        ))}
+    </MainContainer>
   );
 }
 

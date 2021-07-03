900f7cd4be87aa8d5d1c18b0eb1eee5346518f06
wali39 <wali190939@gmail.com>
2021-07-03 16:32:14 +0600

    fix: blog try catch and validation update

--- backend/src/controller/blogs.controller.ts
+++ backend/src/controller/blogs.controller.ts
@@ -8,20 +8,20 @@ import { Blog } from "../entity";
 // @POST - /api/v1/blogs
 // Create a Blog
 export async function createBlog(req: express.Request, res: express.Response) {
-  const permission = accessControl.can(res.locals.user.role).createAny("blog");
-
-  if (!permission.granted)
-    return res.status(403).json({ success: false, error: "Unauthorized" });
-
-  // Validation result
-
-  const { title, author, description } = req.body;
-
-  // Save to database
-  const blogRepository = getConnection().getRepository(Blog);
-  const duplicateCheck = await blogRepository.findOne({ title });
-
   try {
+    const permission = accessControl
+      .can(res.locals.user.role)
+      .createAny("blog");
+
+    if (!permission.granted)
+      return res.status(403).json({ success: false, error: "Not authorized" });
+
+    const { title, author, description } = req.body;
+
+    // Save to database
+    const blogRepository = getConnection().getRepository(Blog);
+    const duplicateCheck = await blogRepository.findOne({ title });
+
     if (!duplicateCheck) {
       let imagePath;
       if (req.file) {
@@ -38,15 +38,15 @@ export async function createBlog(req: express.Request, res: express.Response) {
 
       await blogRepository.save(newBlog);
     } else {
-      res.json({ success: false, error: "Blog already exists" });
+      res.status(400).json({ success: false, error: "Blog already exists" });
       return;
     }
   } catch (err) {
-    res.json({ success: false, error: err });
+    res.status(500).json("Something went wrong");
     return;
   }
 
-  res.json({ success: true, message: "Blog created" });
+  res.status(201).json({ success: true, message: "Blog created" });
 }
 
 // @GET - /api/v1/blogs/
@@ -101,35 +101,29 @@ export async function updateSingleBlog(
       .updateAny("blog");
 
     if (!permission.granted)
-      return res.status(403).json({ success: false, error: "Unauthorized" });
+      return res.status(403).json({ success: false, error: "Not authorized" });
 
     const { title, author, description } = req.body;
     const blogRepository = getConnection().getRepository(Blog);
-    try {
-      let imagePath;
-      if (req.file) {
-        imagePath = req.file.path;
-      }
-      //  else {
-      //   imagePath = "";
-      // }
-      const newBlog = new Blog();
-      newBlog.title = title;
-      newBlog.author = author;
-      newBlog.description = description;
-      if (imagePath) {
-        newBlog.path = imagePath;
-      }
 
-      await blogRepository.update({ id: req.params.blogId }, newBlog);
-    } catch (err) {
-      res.json({
-        success: false,
-        error: "Title must be unique, or Blog can not be updated",
-      });
+    let imagePath;
+    if (req.file) {
+      imagePath = req.file.path;
     }
+    //  else {
+    //   imagePath = "";
+    // }
+    const newBlog = new Blog();
+    newBlog.title = title;
+    newBlog.author = author;
+    newBlog.description = description;
+    if (imagePath) {
+      newBlog.path = imagePath;
+    }
+
+    await blogRepository.update({ id: req.params.blogId }, newBlog);
   } catch (error) {
-    res.status(403).json({ success: false, error: "Internel error" });
+    res.status(500).json("Something went wrong");
     return;
   }
   res.status(200).json({ success: true, message: "Blog is now updated" });
@@ -150,28 +144,36 @@ export async function deleleBlogImage(
       findBlogById.path = "";
       blogRepository.save(findBlogById);
     }
-    res.json({ success: true, message: "blogImage deleted" });
-  } catch (err) {
-    res.json({ success: false, error: "blog have no image" });
+    res.status(200).json({ success: true, message: "blogImage deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "blog have no image" });
   }
 }
 // @DELETE - /api/v1/blogs/:blogId
 // delete blog
 export async function deleteBlog(req: express.Request, res: express.Response) {
-  const id = req.params.blogId;
-  const blogRepository = getConnection().getRepository(Blog);
   try {
     const permission = accessControl
       .can(res.locals.user.role)
       .deleteAny("blog");
 
     if (!permission.granted)
-      return res.status(403).json({ success: false, error: "Unauthorized" });
+      return res.status(403).json({ success: false, error: "Not authorized" });
 
-    if (await blogRepository.delete({ id })) {
-      return res.json({ success: true, message: "Delete successfully" });
+    const id = req.params.blogId;
+    const blogRepository = getConnection().getRepository(Blog);
+    const blogCheck = await blogRepository.findOne({ id });
+
+    if (blogCheck) {
+      await blogRepository.delete({ id });
+      res.status(200).json({ success: true, message: "Delete successfully" });
+    } else {
+      res.status(400).json({
+        success: false,
+        error: "Invalid blogId or blog already deleted",
+      });
     }
-  } catch (err) {
-    res.json({ success: false, error: "Blog is not identified" });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
 }

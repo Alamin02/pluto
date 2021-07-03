8b29b5175fbb270132d8345ee1beaf6f7fc11cfb
wali39 <wali190939@gmail.com>
2021-07-03 17:19:45 +0600

    fix : try catch and validation added

--- backend/src/controller/offers.controller.ts
+++ backend/src/controller/offers.controller.ts
@@ -9,18 +9,22 @@ export async function getAllOffers(
   req: express.Request,
   res: express.Response
 ) {
-  const offersRepository = getConnection().getRepository(Offer);
+  try {
+    const offersRepository = getConnection().getRepository(Offer);
 
-  const [offers, offerCount] = await offersRepository.findAndCount({
-    relations: ["offerImage", "products", "products.images"],
-  });
+    const [offers, offerCount] = await offersRepository.findAndCount({
+      relations: ["offerImage", "products", "products.images"],
+    });
 
-  res.json({
-    data: {
-      offers,
-      offerCount,
-    },
-  });
+    res.json({
+      data: {
+        offers,
+        offerCount,
+      },
+    });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
+  }
 }
 
 // get a offer
@@ -28,50 +32,56 @@ export async function getSingleOffer(
   req: express.Request,
   res: express.Response
 ) {
-  const id = req.params.offerId;
-  const offersRepository = getConnection().getRepository(Offer);
-  const findByOffer = await offersRepository.findOne(
-    { id: id },
-    { relations: ["products"] }
-  );
+  try {
+    const id = req.params.offerId;
+    const offersRepository = getConnection().getRepository(Offer);
+    const findByOffer = await offersRepository.findOne(
+      { id: id },
+      { relations: ["products"] }
+    );
 
-  if (!findByOffer) {
-    res.status(400).json({ errors: [{ msg: "Offer not found" }] });
+    if (!findByOffer) {
+      res.status(400).json({ errors: [{ msg: "Offer not found" }] });
+    }
+    res.status(200).json({ msg: "offer found", data: findByOffer });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
-  res.status(200).json({ msg: "offer found", data: findByOffer });
 }
 
 // @POST /v1/api/offers
 // create offer
 export async function createOffer(req: express.Request, res: express.Response) {
-  const permission = accessControl.can(res.locals.user.role).createAny("offer");
-  if (!permission.granted) {
-    return res.status(403).json({ success: false, error: "Unauthorized" });
-  }
-
-  const { name, discount, description } = req.body;
-
-  const offersRepository = getConnection().getRepository(Offer);
-  const previousEntry = await offersRepository.findOne({ name });
-
-  const offerImageRepository = getConnection().getRepository(OfferImage);
-  const createOfferImage = [];
-  const files = req.files as Express.Multer.File[];
-
-  if (files.length) {
-    for (let i = 0; i < files.length; i++) {
-      const offerImage = new OfferImage();
-      offerImage.path = files[i].path;
-      offerImage.originalname = files[i].originalname;
-
-      const savedProductImage = await offerImageRepository.save(offerImage);
-      createOfferImage.push(savedProductImage);
-    }
-  } else {
-    return res.json({ success: false, error: "Image not found" });
-  }
-
   try {
+    // const permission = accessControl
+    //   .can(res.locals.user.role)
+    //   .createAny("offer");
+    // if (!permission.granted) {
+    //   return res.status(403).json({ success: false, error: "Unauthorized" });
+    // }
+
+    const { name, discount, description } = req.body;
+
+    const offersRepository = getConnection().getRepository(Offer);
+    const previousEntry = await offersRepository.findOne({ name });
+
+    const offerImageRepository = getConnection().getRepository(OfferImage);
+    const createOfferImage = [];
+    const files = req.files as Express.Multer.File[];
+
+    if (files && files.length) {
+      for (let i = 0; i < files.length; i++) {
+        const offerImage = new OfferImage();
+        offerImage.path = files[i].path;
+        offerImage.originalname = files[i].originalname;
+
+        const savedProductImage = await offerImageRepository.save(offerImage);
+        createOfferImage.push(savedProductImage);
+      }
+    } else {
+      return res.json({ success: false, error: "Image not found" });
+    }
+
     if (!previousEntry) {
       const newOffer = new Offer();
       newOffer.name = name;
@@ -81,30 +91,33 @@ export async function createOffer(req: express.Request, res: express.Response) {
 
       await offersRepository.save(newOffer);
 
-      res.status(200).json({ success: true, message: "Offer created" });
+      res.status(201).json({ success: true, message: "Offer created" });
     } else {
       res.status(400).json({ success: false, error: "offer already exist" });
     }
-  } catch (e) {
-    res.status(400).json({ success: false, error: e });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
+    console.log(error);
   }
 }
 
 // @PUT /v1/api/offers/:offerId
 // update a offer
 export async function updateOffer(req: express.Request, res: express.Response) {
-  const offerId = req.params.offerId;
-  const { name, discount, description } = req.body;
-
-  const offersRepository = getConnection().getRepository(Offer);
-  const offerToUpdate = await offersRepository.findOne({ id: offerId });
-
-  const permisson = accessControl.can(res.locals.user.role).updateAny("offer");
-  if (!permisson.granted) {
-    return res.status(403).json({ success: false, error: "Unauthorized" });
-  }
-
   try {
+    const offerId = req.params.offerId;
+    const { name, discount, description } = req.body;
+
+    const offersRepository = getConnection().getRepository(Offer);
+    const offerToUpdate = await offersRepository.findOne({ id: offerId });
+
+    // const permisson = accessControl
+    //   .can(res.locals.user.role)
+    //   .updateAny("offer");
+    // if (!permisson.granted) {
+    //   return res.status(403).json({ success: false, error: "Unauthorized" });
+    // }
+
     if (offerToUpdate) {
       const newOffer = new Offer();
       newOffer.name = name;
@@ -113,7 +126,7 @@ export async function updateOffer(req: express.Request, res: express.Response) {
       offersRepository.merge(offerToUpdate, newOffer);
       await offersRepository.save(offerToUpdate);
 
-      res.status(200).json({ success: true, message: "offer updated" });
+      res.status(201).json({ success: true, message: "offer updated" });
     } else {
       res.status(400).json({
         success: false,
@@ -121,25 +134,26 @@ export async function updateOffer(req: express.Request, res: express.Response) {
           "offer name must be unique or offer to update not found or invalid id",
       });
     }
-  } catch (e) {
-    res
-      .status(400)
-      .json({ success: false, error: "offer name must be unique" });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
 }
 
 // @DELETE /v1/api/offers/:offerId
 // delete a offer
 export async function deleteOffer(req: express.Request, res: express.Response) {
-  const permisson = accessControl.can(res.locals.user.role).deleteAny("offer");
-  if (!permisson.granted) {
-    return res.status(403).json({ success: false, error: "Unauthorized" });
-  }
-
-  const offerId = req.params.offerId;
-  const offersRepository = getConnection().getRepository(Offer);
-  const offerToUpdate = await offersRepository.findOne({ id: offerId });
   try {
+    // const permisson = accessControl
+    //   .can(res.locals.user.role)
+    //   .deleteAny("offer");
+    // if (!permisson.granted) {
+    //   return res.status(403).json({ success: false, error: "Unauthorized" });
+    // }
+
+    const offerId = req.params.offerId;
+    const offersRepository = getConnection().getRepository(Offer);
+    const offerToUpdate = await offersRepository.findOne({ id: offerId });
+
     if (offerToUpdate) {
       await offersRepository.delete(offerId);
       res.status(200).json({ success: true, message: "Offer deleted" });
@@ -149,7 +163,7 @@ export async function deleteOffer(req: express.Request, res: express.Response) {
         error: "offer to delete not found or invalid id",
       });
     }
-  } catch (e) {
-    res.status(400).json({ success: false, error: e });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
 }

e1800e063760eda7477e591a4dd8cbb22e22b9e9
wali39 <wali190939@gmail.com>
2021-07-07 01:14:55 +0600

    fix: return , try,catch add and duplicate checked

--- backend/src/controller/blogs.controller.ts
+++ backend/src/controller/blogs.controller.ts
@@ -20,9 +20,9 @@ export async function createBlog(req: express.Request, res: express.Response) {
 
     // Save to database
     const blogRepository = getConnection().getRepository(Blog);
-    const duplicateCheck = await blogRepository.findOne({ title });
+    const duplicate = await blogRepository.findOne({ title });
 
-    if (!duplicateCheck) {
+    if (!duplicate) {
       let imagePath;
       if (req.file) {
         imagePath = req.file.path;
@@ -37,41 +37,47 @@ export async function createBlog(req: express.Request, res: express.Response) {
       newBlog.path = imagePath;
 
       await blogRepository.save(newBlog);
+      return res
+        .status(201)
+        .json({ success: true, message: "New blog added!" });
     } else {
-      res.status(400).json({ success: false, error: "Blog already exists" });
-      return;
+      return res
+        .status(400)
+        .json({ success: false, error: "Blog title already exists" });
     }
   } catch (err) {
-    res.status(500).json("Something went wrong");
-    return;
+    return res.status(500).json("Something went wrong");
   }
-
-  res.status(201).json({ success: true, message: "Blog created" });
 }
 
 // @GET - /api/v1/blogs/
 // Get All Blogs
 export async function getAllBlogs(req: express.Request, res: express.Response) {
-  const blogRepository = getConnection().getRepository(Blog);
+  try {
+    const blogRepository = getConnection().getRepository(Blog);
 
-  const page = parseInt(<string>req.query.page);
-  const perPage = parseInt(<string>req.query.perPage);
+    const page = parseInt(<string>req.query.page);
+    const perPage = parseInt(<string>req.query.perPage);
 
-  const [blogs, blogCount] = await blogRepository.findAndCount({
-    select: ["id", "title", "author", "description", "path"],
-    take: page * perPage,
-    skip: (page - 1) * perPage,
-  });
+    const [blogs, blogCount] = await blogRepository.findAndCount({
+      select: ["id", "title", "author", "description", "path"],
+      take: page * perPage,
+      skip: (page - 1) * perPage,
+    });
 
-  res.json({
-    data: {
-      blogs,
-      blogCount,
-      currentPage: page,
-      maxPages: Math.ceil(blogCount / perPage),
-      perPage,
-    },
-  });
+    return res.status(200).json({
+      success: true,
+      data: {
+        blogs,
+        blogCount,
+        currentPage: page,
+        maxPages: Math.ceil(blogCount / perPage),
+        perPage,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json("Something went wrong ");
+  }
 }
 
 // @GET - /api/v1/blogs/:blogId
@@ -80,13 +86,19 @@ export async function getSingleBlog(
   req: express.Request,
   res: express.Response
 ) {
-  const id = req.params.blogId;
-  const blogRepository = getConnection().getRepository(Blog);
-  const singleBlog = await blogRepository.findOne({ id });
-  if (!singleBlog) {
-    return res.status(400).json({ success: false, error: "Blog is not found" });
+  try {
+    const id = req.params.blogId;
+    const blogRepository = getConnection().getRepository(Blog);
+    const singleBlog = await blogRepository.findOne({ id });
+    if (!singleBlog) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Blog is not found" });
+    }
+    return res.status(200).json({ success: true, data: singleBlog });
+  } catch (error) {
+    return res.status(500).json("Something went wrong");
   }
-  res.status(200).json({ data: singleBlog });
 }
 
 // @PUT - /api/v1/blogs/:blogId
@@ -107,39 +119,41 @@ export async function updateSingleBlog(
     const blogId = req.params.blogId;
     const blogRepository = getConnection().getRepository(Blog);
     const blogCheck = await blogRepository.findOne({ id: blogId });
-    const duplicateCheck = await blogRepository.findOne({ title });
+    const duplicate = await blogRepository.findOne({ title });
 
     if (blogCheck) {
-      if (!duplicateCheck) {
-        let imagePath;
-        if (req.file) {
-          imagePath = req.file.path;
-        }
-        //  else {
-        //   imagePath = "";
-        // }
-        const newBlog = new Blog();
-        newBlog.title = title;
-        newBlog.author = author;
-        newBlog.description = description;
-        if (imagePath) {
-          newBlog.path = imagePath;
-        }
-
-        await blogRepository.update({ id: blogId }, newBlog);
-      } else {
-        res.status(400).json({ success: false, error: "Blog already exists" });
+      if (duplicate && duplicate.id !== blogId) {
+        return res
+          .status(400)
+          .json({ success: false, error: "Blog title already exists" });
       }
+      let imagePath;
+      if (req.file) {
+        imagePath = req.file.path;
+      }
+      //  else {
+      //   imagePath = "";
+      // }
+      const newBlog = new Blog();
+      newBlog.title = title;
+      newBlog.author = author;
+      newBlog.description = description;
+      if (imagePath) {
+        newBlog.path = imagePath;
+      }
+
+      await blogRepository.update({ id: blogId }, newBlog);
+      return res
+        .status(200)
+        .json({ success: true, message: "Blog is now updated" });
     } else {
-      res
+      return res
         .status(400)
         .json({ success: false, error: "Invalid blogId or blog deleted" });
     }
   } catch (error) {
-    res.status(500).json("Something went wrong");
-    return;
+    return res.status(500).json("Something went wrong");
   }
-  res.status(200).json({ success: true, message: "Blog is now updated" });
 }
 
 // @DELETE - /api/v1/blogs/blogImage/:blogId
@@ -148,18 +162,22 @@ export async function deleleBlogImage(
   req: express.Request,
   res: express.Response
 ) {
-  const id = req.params.blogId;
-  const blogRepository = getConnection().getRepository(Blog);
-
   try {
+    const id = req.params.blogId;
+    const blogRepository = getConnection().getRepository(Blog);
+
     const findBlogById = await blogRepository.findOne({ id });
     if (findBlogById && findBlogById.path) {
       findBlogById.path = "";
       blogRepository.save(findBlogById);
     }
-    res.status(200).json({ success: true, message: "blogImage deleted" });
+    return res
+      .status(200)
+      .json({ success: true, message: "blogImage deleted" });
   } catch (error) {
-    res.status(500).json({ success: false, error: "blog have no image" });
+    return res
+      .status(500)
+      .json({ success: false, error: "blog have no image" });
   }
 }
 // @DELETE - /api/v1/blogs/:blogId
@@ -173,20 +191,22 @@ export async function deleteBlog(req: express.Request, res: express.Response) {
     if (!permission.granted)
       return res.status(403).json({ success: false, error: "Not authorized" });
 
-    const id = req.params.blogId;
+    const blogId = req.params.blogId;
     const blogRepository = getConnection().getRepository(Blog);
-    const blogCheck = await blogRepository.findOne({ id });
+    const blogCheck = await blogRepository.findOne({ id: blogId });
 
     if (blogCheck) {
-      await blogRepository.delete({ id });
-      res.status(200).json({ success: true, message: "Delete successfully" });
+      await blogRepository.delete({ id: blogId });
+      return res
+        .status(200)
+        .json({ success: true, message: "Delete successfully" });
     } else {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         error: "Invalid blogId or blog already deleted",
       });
     }
   } catch (error) {
-    res.status(500).json("Something went wrong");
+    return res.status(500).json("Something went wrong");
   }
 }

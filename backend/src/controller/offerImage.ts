f51e548f69759814ddd907a628cb115f63c700c2
wali39 <wali190939@gmail.com>
2021-07-07 13:45:26 +0600

    fix: try, catch, return statement,  error msg add

--- backend/src/controller/offerImage.ts
+++ backend/src/controller/offerImage.ts
@@ -1,12 +1,12 @@
-import express = require("express")
+import express = require("express");
 import { getConnection } from "typeorm";
 import { OfferImage, Offer } from "../entity";
 
 // get offer Images
 export async function getAllOfferImages(
   req: express.Request,
-  res: express.Response) {
-
+  res: express.Response
+) {
   // write function
 }
 
@@ -15,59 +15,68 @@ export async function createOfferImage(
   req: express.Request,
   res: express.Response
 ) {
-  const id = req.body.offerId;
-  const offerRepository = getConnection().getRepository(Offer);
-  const offer = await offerRepository.findOne({ id });
-  const offerImageRepository = getConnection().getRepository(OfferImage);
+  try {
+    const id = req.body.offerId;
+    const offerRepository = getConnection().getRepository(Offer);
+    const offer = await offerRepository.findOne({ id });
+    const offerImageRepository = getConnection().getRepository(OfferImage);
 
-  const createOfferImage = [];
-  const files = req.files as Express.Multer.File[];
+    const createOfferImage = [];
+    const files = req.files as Express.Multer.File[];
 
-  if (files.length && offer) {
-    for (let i = 0; i < files.length; i++) {
-      const offerImage = new OfferImage();
-      offerImage.path = files[i].path;
-      offerImage.originalname = files[i].originalname;
-      offerImage.offer = offer;
+    if (files.length && offer) {
+      for (let i = 0; i < files.length; i++) {
+        const offerImage = new OfferImage();
+        offerImage.path = files[i].path;
+        offerImage.originalname = files[i].originalname;
+        offerImage.offer = offer;
 
-      const savedOfferImage = await offerImageRepository.save(offerImage);
-      createOfferImage.push(savedOfferImage);
+        const savedOfferImage = await offerImageRepository.save(offerImage);
+        createOfferImage.push(savedOfferImage);
+      }
+
+      return res
+        .status(200)
+        .json({ success: true, message: "OfferImage added!" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, error: "OfferImage not found!" });
     }
-
-    res.json({ msg: "Image added" });
-  } else {
-    return res.status(400).json({ errors: [{ msg: "Image not found" }] });
+  } catch (error) {
+    return res.status(500).json("Something went wrong!");
   }
-
 }
 
 // update offer Image
 export async function updateOfferImage(
   req: express.Request,
   res: express.Response
-) {
+) {}
 
-}
-
-// delete offer Image 
+// delete offer Image
 export async function deleteOfferImage(
   req: express.Request,
   res: express.Response
 ) {
-  const Id = req.params.offerImageId;
-  const offerImageRepository = getConnection().getRepository(OfferImage);
-  const imageToUpdate = await offerImageRepository.findOne({ id: Id });
-  if (imageToUpdate) {
-    try {
-      await offerImageRepository.delete(Id);
-      res.json({ msg: "image deleted" });
-    } catch (e) {
-      res.status(400).json({ msg: e });
+  try {
+    const Id = req.params.offerImageId;
+    const offerImageRepository = getConnection().getRepository(OfferImage);
+    const imageToUpdate = await offerImageRepository.findOne({ id: Id });
+    if (imageToUpdate) {
+      try {
+        await offerImageRepository.delete(Id);
+        res.status(200).json({ success: true, message: "OfferImage deleted!" });
+      } catch (e) {
+        return res.status(500).json("something went wrong");
+      }
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid offerImagId!",
+      });
     }
-  } else {
-    res.status(400).json({
-      errors: [{ msg: "Offer image to delete not found or invalid id" }],
-    });
+  } catch (error) {
+    return res.status(500).json("Something went wrong!");
   }
-
 }

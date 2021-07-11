4c693071cbfbe3116bc017f3bcf6be63947e098b
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-07-11 11:59:01 +0600

    fix: order validators updated

--- backend/src/validators/orders.validator.ts
+++ backend/src/validators/orders.validator.ts
@@ -1,7 +1,10 @@
 import Joi from "joi";
 
 export const createOrderSchema = Joi.object({
+  user: Joi.object().required(),
+  orderedProducts: Joi.array().required(),
   paymentMethod: Joi.string().required(),
+  address: Joi.object().required(),
 });
 
 export const updateOrderSchema = Joi.object({
